0bdc28d5e06a3e302572057571a1ab553e0235fb
Matheus-Oliveira-Pereira <ma.pe.oli.1998@gmail.com>
2021-12-13 17:44:20 -0300

    foi realizada componentizacao requeridas no desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  onClick:(id: number) => void;
+  selectedGenreId: number;
+}
+
+
+export const SideBar:React.FC<SideBarProps> = ({ genres, selectedGenreId, onClick }:SideBarProps) => {
+  return(
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => onClick(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  );  
 }
